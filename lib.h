d1ada79ade42dd404827ea77fb42c45e672096d1
GabrielPereiraPinto <arkanjogabriel7@hotmail.com>
2018-03-18 10:58:03 -0300

    fix 5

--- lib.h
+++ lib.h
@@ -1,3 +1,4 @@
 long int convertToTen (char *NumeroInicial, int baseAtual );
 int convertToDecimal (char digito);
 void convertToBase(long int NumeroBase10, int baseDesejada);
+int Exponencial(int numero, int expoente);
