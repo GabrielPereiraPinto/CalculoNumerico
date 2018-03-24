67371c20ebc4c26e67a0bac947fe69fa4b8106e5
GabrielPereiraPinto <arkanjogabriel7@hotmail.com>
2018-03-24 12:31:37 -0300

    Função de criar Seno e Cosseno dat

--- senac-cn-ado02/trig.c
+++ senac-cn-ado02/trig.c
@@ -7,4 +7,34 @@
 
 int main() {
 
+
+	FILE *filePath;
+	filePath = fopen("seno.dat", "a");
+	filePath = fopen("seno.dat", "b");
+
+	for (int i = 0; i < 721; i++) {
+
+		double rad = i * (pi / 180);
+		double erroAbsoluto = fabs(sin(rad) - sen(rad));
+		double erroRelativo = fabs(erroAbsoluto / sen(rad));
+		fprintf(filePath, "%d\t%f\t%f\t%f\n", i, sen(rad), erroAbsoluto, erroRelativo);
+	}
+
+	fclose(filePath);
+
+	filePath = fopen("cosseno.dat", "a");
+	filePath = fopen("cosseno.dat", "b");
+
+	for (int i = 0; i < 721; i++) {
+
+		double rad = i * (pi / 180);
+		double erroAbsoluto = fabs(cos(rad) - cos(rad));
+		double erroRelativo = fabs(erroAbsoluto / cos(rad));
+		fprintf(filePath, "%d\t%f\t%f\t%f\n", i, cos(rad), erroAbsoluto, erroRelativo);
+	}
+
+	fclose(filePath);
+
+	free(filePath);
+
 }
